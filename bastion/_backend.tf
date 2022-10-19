8e07bcf66e44f346bc7ba05921fa3e1ad2b18c2f
rkdtks0816 <72867019+rkdtks0816@users.noreply.github.com>
2022-10-19 16:36:55 +0900

    Update _backend.tf

--- bastion/_backend.tf
+++ bastion/_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket      = "gj-cicd"
+    bucket      = "songsong0816"
     key         = "dev/apne2/ec2/bastion/terraform.tfstate"
     region      = "ap-northeast-2"
 #    role_arn    = "{ASSUMED_ROLE}"
