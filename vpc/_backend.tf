22ce3b6286c38c245a12fba192ac3e9026c2af55
rkdtks0816 <72867019+rkdtks0816@users.noreply.github.com>
2022-10-19 20:39:14 +0900

    Update _backend.tf

--- vpc/_backend.tf
+++ vpc/_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket      = "songsong0816"
-    key         = "dev/apne2/network/vpc/terraform.tfstate"
-    region      = "ap-northeast-2"
+    bucket      = local.bucket
+    key         = local.key
+    region      = local.region
 #    role_arn    = "{ASSUMED_ROLE}"
-    max_retries = 3
+    max_retries = local.max_retries
   }
 }
